550f9ee3307dbb0448393c9707964ccffa473923
Saurabh Dahibhate <saurabhdahibhate50@gmail.com>
2025-10-09 19:58:03 +0530

    fix: add CORS for production

--- server/server.js
+++ server/server.js
@@ -10,7 +10,15 @@ import youtubeRoutes from "./routes/youtubeRoutes.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://student-revision-app-two.vercel.app/",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 dotenv.config();
 
